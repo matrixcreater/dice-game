59e78fff12300e8892d96b20d9588abb5ff9052d
matrixcreater <76240187+matrixcreater@users.noreply.github.com>
2020-12-19 11:48:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var address="images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",address);
+var address2="images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",address2);
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩️ Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩️ Player 2 Wins";
+}
+else 
+{
+  document.querySelector("h1").innerHTML="Draw";
+}
